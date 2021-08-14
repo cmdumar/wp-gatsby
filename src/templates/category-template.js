d796e7593adde780286b072cbf2ebcec639ed1c3
Muhammad Umar <mohammadumar28@gmail.com>
2021-08-14 16:58:40 +0530

    Style the header of category pages

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+    Image, Heading, Box
+  } from "@chakra-ui/react";
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
@@ -38,8 +41,30 @@ const CategoryTemplate = ({ data }) => {
 
     return <>
         <Layout>
-            <h1 dangerouslySetInnerHTML={{ __html: title }} />
-            <img src={bg.sourceUrl} alt={bg.altText} />
+            <Box
+                width="full"
+                position="relative"  
+            >
+                <Image
+                    src={bg.sourceUrl}
+                    alt={bg.altText}
+                    objectFit="cover"
+                    width="full"
+                    height="60vh"
+                    alt="Background"
+                />
+                <Heading
+                    position="absolute"
+                    top="70%"
+                    left="0"
+                    right="0"
+                    textAlign="center"
+                    color="green.900"
+                    textShadow="1px 1px 0 #57ff64"
+                    size="3xl"
+                    dangerouslySetInnerHTML={{ __html: title }}
+                />
+            </Box>
             <div>
                 {posts.map(post => (
                     <div key={post.id} style={{ border: "1px solid black", padding: "30px", margin: "10px" }}>
